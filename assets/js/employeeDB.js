cd6cc3bef88da709c07421c017aaa9de8ab071e5
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-05-17 21:01:01 -0400

    sum

--- assets/js/employeeDB.js
+++ assets/js/employeeDB.js
@@ -14,35 +14,58 @@ const sqlSelect = (type)=>{
    switch (type) {
 
      case 0 :  // all department
-      return `select name as dept 
+      return `select id, name as dept 
               from department`;
      case 1 : // all roles
-      return `select r.title as role, CONCAT('$ ', FORMAT(r.salary, 0)) as salary, ifnull(d.name, '') as dept 
+      return `select r.id, r.title as role, CONCAT('$ ', FORMAT(r.salary, 0)) as salary, ifnull(d.name, '') as dept 
               from role as r
               left join department as d on department_id = d.id`;
      case 2 :  // all employees
-      return  `select e.first_name as name, e.last_name as 'last name', r.title as role, ifnull(m.first_name,'') as manager 
+      return  `select e.id, e.first_name as name, e.last_name as 'last name', r.title as role, ifnull(m.first_name,'') as manager 
                from employee as e 
                left join role as r on e.role_id = r.id 
                left join employee as m on e.manager_id = m.id`;
-     case 3 :
+     case 3 :  // add dept
        return `insert into department (name) value (?)`;
-     case 4 :
+     case 4 :  // add role
         return `insert into role (title, salary, department_id) 
                 value (?, ?, ?)`;
-     case 5 :
+     case 5 :  // add employee
         return  `insert into employee (first_name, last_name, role_id, manager_id)
                  value (?, ?, ?, ?)`; 
-     case 6 :
+     case 6 :  // update employee role
          return `UPDATE employee
-                 SET rol = ?
+                 SET role_id = ?
                  WHERE id = ?`;
-     case 7 :
+     case 7 :  // update employee manager
        return  `UPDATE employee
-                SET manager = ?
+                SET manager_id = ?
                 WHERE id = ?`;
-
-   }
+     case 8 :  // all employees
+        return  `select e.id, e.first_name as name, e.last_name as 'last name', r.title as role, ifnull(m.first_name,'') as manager 
+                  from employee as e 
+                  left join role as r on e.role_id = r.id 
+                  left join employee as m on e.manager_id = m.id
+                  where e.manager_id = ?`;
+     case 9 :  // delete dept
+        return `delete from department 
+                where id = ?`;
+     case 10 : // delete role
+        return `delete from role 
+                where id = ?`;
+     case 11 :  // delete employee
+        return  `delete from employee
+                 where id = ?`;
+     case 12 :  // Budget by dept and total in the last row
+        return  `select 
+                  case grouping(d.name) when 1 then 'Total' else d.name end as dept, 
+                  CONCAT('$ ', FORMAT(sum(r.salary), 0))  as budget
+                  from employee as e 
+                  left join role as r on e.role_id = r.id 
+                  left join department as d on d.id = r.department_id
+                  group by d.name
+                  with rollup ;`;
+   } 
   }
 
 //query select in the db
@@ -75,5 +98,5 @@ conn.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${conn.threadId}\n`);
   //requested operation 
-  renderQuery(sqlSelect(0),data);
+  renderQuery(sqlSelect(12),data);
 });
